e52b48acd92778b6f1ede6a9a1e72963fb9954bc
karivarkey <geevargheseregi78@gmail.com>
2024-01-27 20:58:24 +0530

    Home page layout and theme and design completed

---
+++ ASK CRUSH/script.js
@@ -0,0 +1,12 @@
+function yes() {
+    document.getElementById('yes').style.display = 'none';
+    document.getElementById('ask').textContent = 'Yippieee! ILY <3';
+}
+
+
+function no() {
+    console.log("guck")
+    document.getElementById('no').style.opacity = '0';
+    document.getElementById('no').style.display = 'none';
+    document.getElementById('ask').textContent = " what!??? what if I say , PRETTY PLEASE <3 ?"
+}
