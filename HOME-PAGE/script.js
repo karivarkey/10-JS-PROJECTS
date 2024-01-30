e4d26f5cc008e3af4a6b488cce88bc883ab9d30e
karivarkey <geevargheseregi78@gmail.com>
2024-01-30 17:13:22 +0530

    Fixed most alignments (I learnt FLEX)

--- HOME-PAGE/script.js
+++ HOME-PAGE/script.js
@@ -1,18 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.card').forEach(function (card) {
+    document.querySelectorAll('.element').forEach(function (card) {
         card.style.transition = 'border-color 0.3s ease-in-out'; // Add CSS transition property
 
         card.addEventListener('mouseover', function () {
             // Generate a random hex color
-            const colors = ["#8839ef", "#53a941", "#fe640b", "#04a5e5", "#303446"];
+            const colors = ["#8839ef", "#53a941", "#fe640b", "#04a5e5",];
             const randomElement = colors[Math.floor(Math.random() * colors.length)];
             // Apply the random color to the border
             this.style.borderColor = randomElement;
         });
-
         card.addEventListener('mouseout', function () {
             // Reset the border color on mouseout
-            this.style.borderColor = 'transparent';
+            this.style.borderColor = " rgba(255, 255, 255, 0.3)";
         });
     });
 });
